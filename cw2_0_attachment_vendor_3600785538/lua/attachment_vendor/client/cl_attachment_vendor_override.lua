1c34e8da505f0fab2de05e3cce161d95b4ccc955
Pauluaner <80461090+Pauluaner@users.noreply.github.com>
2026-02-21 16:50:15 +0100

    Delete cw2_0_attachment_vendor_3600785538 directory

--- cw2_0_attachment_vendor_3600785538/lua/attachment_vendor/client/cl_attachment_vendor_override.lua
+++
@@ -1,6 +0,0 @@
-if (not ATTACHMENT_VENDOR.override.enable) then return; end
-
--- This was missing from CW2
-usermessage.Hook("CW20_REMOVEATTACHMENT", function(d)
-   LocalPlayer().CWAttachments[d:ReadString()] = nil;
-end);
--- cw2_0_attachment_vendor_3600785538/lua/attachment_vendor/server/sv_attachment_vendor_ready.lua
+++
@@ -1,6 +0,0 @@
-util.AddNetworkString("attvend_ready");
-
-net.Receive("attvend_ready", function(l, p)
-   p.attvend_ready = true;
-   gamemode.Call("PlayerLoadout", p);
-end);
